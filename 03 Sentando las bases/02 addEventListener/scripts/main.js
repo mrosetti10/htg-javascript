3eb432220d0f14a9e9c5cffee80653ac6fe644a9
mrosetti10 <micaelarosetti10@gmail.com>
2020-01-13 17:06:18 -0300

    resolví ejercicio 1

--- 03 Sentando las bases/02 addEventListener/scripts/main.js
+++ 03 Sentando las bases/02 addEventListener/scripts/main.js
@@ -9,8 +9,11 @@ function recibirNombre(event) {
 }
 
 function interceptarNombre(event) {
-  console.log(event.target.value);
-}
+console.log(event.target.value);
+if (nombre.lenght < 5|| nombre.lenght)
 
+
+  
+if (nombre < 5 || nombre>50)
 formulario.addEventListener('submit', recibirNombre);
 inputNombre.addEventListener('input', interceptarNombre);
