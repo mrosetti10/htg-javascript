608477907d6e2715d19e8fc65580e854b3d01919
mrosetti10 <micaelarosetti10@gmail.com>
2020-01-13 15:26:11 -0300

    me bajé las actualizaciones del repo

--- 03 Sentando las bases/01 querySelector/scripts/main.js
+++ 03 Sentando las bases/01 querySelector/scripts/main.js
@@ -1,1 +1,8 @@
 console.info('Cuando veas éste mensaje en consola, ya habrás terminado ésta parte del ejercicio. Borra ésta línea cuando ya la hayas visto alguna vez en la consola al acceder a ésta página.')
+var comentarios = document.querySelectorAll('.comment--text') ;
+comentarios[2].style.fontSize="30px"
+var estilo = document.querySelectorAll('hr') ;
+for ( i=0;i<estilo.length;i++){
+    estilo[i].classList.add('.colorido');
+    }
+    
