088973d04d154c0920a7a3f2531a7e8c56f97371
mrosetti10 <micaelarosetti10@gmail.com>
2020-01-16 16:49:47 -0300

    Resolví ejercicio 1 y 2

--- 03 Sentando las bases/02 addEventListener/scripts/main.js
+++ 03 Sentando las bases/02 addEventListener/scripts/main.js
@@ -2,18 +2,32 @@
 
 const formulario = document.querySelector('#contacto');
 const inputNombre = document.querySelector('#input-nombre');
+const mensaje = document.querySelector('.mensaje');
+const inputsolucion = document.querySelector('#input-solucion');
 
 function recibirNombre(event) {
+  console.log('hola')
   event.preventDefault();
-  console.log(event);
+  formulario.textContent = 'Se enviaron los datos';
+  formulario.style.color = 'blue';
+
 }
 
+
 function interceptarNombre(event) {
-console.log(event.target.value);
-if (nombre.lenght < 5|| nombre.lenght)
 
 
-  
-if (nombre < 5 || nombre>50)
+  const nombre = event.target.value;  
+   if(nombre.lenght < 5 || nombre.lenght > 50 ){
+   mensaje.textContent = 'Supera la cantidad';
+   mensaje.style.color = "green";
+   console.log('no es valido'); 
+ }else{
+   mensaje.textContent='';
+
+    }
+}
+
 formulario.addEventListener('submit', recibirNombre);
 inputNombre.addEventListener('input', interceptarNombre);
+mensaje.addEventListener('span', interceptarNombre); //acá le estoy diciendo que es un evento
